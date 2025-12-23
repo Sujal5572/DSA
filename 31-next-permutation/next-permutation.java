4bec78e8dfe32e87e2099d7cf921f96c9e4baf50
Sujal5572 <138520969+Sujal5572@users.noreply.github.com>
2025-12-23 19:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (37.61%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,14 +1,16 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length-2;
+        //here we are finding the dip from index n-1 to 0
         while(i>=0 && nums[i]>=nums[i+1]){
         i--;
     }
      // we get the index i where there is dip
+     //if last max permu is there then we will get i=-1 and then we can just reverse the number
     if(i>=0){
     int j=nums.length-1;
     while(j>=0 && nums[j]<=nums[i] )j--;
-    // we get the index of numner just greater than nums[i]
+    // we get the index of number just greater than nums[i]
 
     // swap the number at i and j index
     swap(nums,i,j);
