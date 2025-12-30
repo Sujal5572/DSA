b752a9fa3584c4c55df7807525b676398c76218f
Sujal5572 <138520969+Sujal5572@users.noreply.github.com>
2025-12-30 17:03:13 +0530

    Time: 1 ms (73.61%) | Memory: 46.7 MB (37.74%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,25 +1,26 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
-        int i=0;
-        int j=1;
-        while(j<n){
+        // int i=0;
+        // int j=1;
+        // while(j<n){
 
-            if(nums[i]!=nums[j]){ // if not same then store value at j to i index and then i++
-            i++;
-            nums[i]=nums[j];
-            }
-            j++;
-        }
-        return i+1;
-        // int c =0;
-        // for(int i=0;i<n;i++){
-        //     if(i<n-1 && nums[i]==nums[i+1]  )
-        //     continue;
-        //     nums[c]=nums[i];
-        //     c++;
+        //     if(nums[i]!=nums[j]){ // if not same then store value at j to i index and then i++
+        //     i++;
+        //     nums[i]=nums[j];
+        //     }
+        //     j++;
         // }
-        // return c;
+        // return i+1;
+
+        int c =0;
+        for(int i=0;i<n;i++){
+            if(i<n-1 && nums[i]==nums[i+1]  )
+            continue;
+            nums[c]=nums[i];
+            c++;
+        }
+        return c;
 
 
         // Set<Integer> s = new HashSet<>();
