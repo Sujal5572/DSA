b67d0220e7a194eebde0eef3eac957e7a5d5befc
Sujal5572 <138520969+Sujal5572@users.noreply.github.com>
2026-01-06 10:26:35 +0530

    Time: 7 ms (98.90%) | Memory: 46.7 MB (25.15%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,16 +9,19 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
+        // tc O(n)as we are traversing each node once 
+        // sc O(h)recursion stack h is the height of the tree
         if(root == null || root == p || root ==q)// base condtion 
-        return root;
+        return root;// if we encounter any one p or q we simple return root 
         TreeNode left= lowestCommonAncestor(root.left, p,q); // left 
         TreeNode right = lowestCommonAncestor(root.right,p,q);//right
-        if(left !=null && right != null){
-            return root;
-        }
-        return left != null ? left : right;
-
+      
+      if(left == null)return right; //in case of p=6 & q=4 we encounter that left of 2 is null so we return right as we get root = q 
+      else if(right == null)return left;
+      else{ // If p and q are found in different subtrees of a node, that node is the LCA.
+        return root; // like p and q both are not null then we are going return root 3 for p=5 , q=1;
+      }
+      // if one is an ancestor of other node then ancestor itself is lca
 
 
         
