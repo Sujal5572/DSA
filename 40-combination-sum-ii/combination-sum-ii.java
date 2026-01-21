900ca2eb442912a00c91953bfb10ddc7b47728fd
Sujal5572 <138520969+Sujal5572@users.noreply.github.com>
2026-01-21 15:54:27 +0530

    Time: 5 ms (99.04%) | Memory: 45.3 MB (58.34%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,5 +1,7 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // tc will be O(2^n) as we are generating all the combination
+        // sc will be O(n) recursion stack space.
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
         help(0,candidates, target , ans , new ArrayList<>());
@@ -14,8 +16,7 @@ class Solution {
         if(t<0)
         return;
         for(int i = ind ;i<c.length;i++){
-            if(i!=ind && c[i]==c[i-1])
-            continue;
+            if(i>ind && c[i]==c[i-1])continue;
             if(c[i]>t)break;
             part.add(c[i]);
             help(i+1,c,t-c[i],ans , part);
