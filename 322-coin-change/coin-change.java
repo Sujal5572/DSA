06ace522b90bbc45b72ff62cbebd2a10bc7ba8b7
Sujal5572 <138520969+Sujal5572@users.noreply.github.com>
2026-01-22 17:02:16 +0530

    Time: 19 ms (40.07%) | Memory: 47.2 MB (23.60%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,34 +1,50 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][]= new int[n][amount+1];
+        int dp[][]= new int[n+1][amount+1];
         int INF = (int)1e9;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n;i++)
         dp[i][0]=0; // we can make amount without taking any coins
-        for(int j=1;j<=amount;j++){
-            // we can only make amount if it is divisible by coin[0]
-            if(j%coins[0]==0) // 0th row will contians the number coins that are required to make that sum from 1 to amount ; 
-                dp[0][j]=j/coins[0];
-            else
-                dp[0][j] = INF;
-
+        for(int j=0;j<=amount;j++){
+            //impossible to make sum with 0 coin
+            dp[0][j] = INF;
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=amount;j++){
-                // if we skip then we are going to pick from previous row
-                int notpick = dp[i-1][j];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount ;j++){
 
-                int pick = INF;
-                if(coins[i]<=j)
-                // 1 + same row , previous amount
-                pick = 1+ dp[i][j-coins[i]]; // we can pick the same number again
-                dp[i][j] = Math.min(pick , notpick);
+                dp[i][j] = dp[i-1][j];
+                if(coins[i-1]<=j){
+                    dp[i][j] = Math.min(dp[i-1][j] ,1 + dp[i][j-coins[i-1]]);// take same coin and. decrease the amount
+                }
             }
         }
-        int res = dp[n-1][amount];
-        return res >= INF ? -1 : res;
+        return dp[n][amount] >= INF ? -1 : dp[n][amount];
     }
 }
+        // for(int j=1;j<=amount;j++){
+        //     // we can only make amount if it is divisible by coin[0]
+        //     if(j%coins[0]==0) // 0th row will contians the number coins that are required to make that sum from 1 to amount ; 
+        //         dp[0][j]=j/coins[0]; // how many coins is used to make that sum
+        //     else
+        //         dp[0][j] = INF; // as we need pick minimum
+
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<=amount;j++){
+        //         // if we skip then we are going to pick from previous row
+        //         int notpick = dp[i-1][j];
+
+        //         int pick = INF;
+        //         if(coins[i]<=j)
+        //         // 1 + same row , previous amount
+        //         pick = 1+ dp[i][j-coins[i]]; // we can pick the same number again
+        //         dp[i][j] = Math.min(pick , notpick);
+        //     }
+        // }
+//         int res = dp[n-1][amount];
+//         return res >= INF ? -1 : res;
+//     }
+// }
 
 
 
@@ -50,7 +66,6 @@ class Solution {
 //         pick = 1 + solve(n , coins , amount - coins[n],dp);
 //         return dp[n][amount] = Math.min(pick , notpick);
 //     }
-    
 // }
 
 
