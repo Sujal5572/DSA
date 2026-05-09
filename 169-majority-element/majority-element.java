5f8b3c1a331df251abe03dbf761e6840a6f98835
Sujal5572 <138520969+Sujal5572@users.noreply.github.com>
2026-05-09 15:43:25 +0530

    Time: 15 ms (22.61%) | Memory: 52.5 MB (90.54%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,40 +1,32 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        int count=0; // storing count of element
-        int ele=0; // element
-        for(int i :nums){
-            if(count==0){ // if count becomes zeros after cancelling out with different element
-            count =1;  // then we should initialized count with 1 and
-            ele= i; // then we ahould store that in ele.
-            }
-            else if(ele==i) // if ele is same then we shoyuld increment the counter;
-            count++;
-            else
-            count--; // if different ele encounter then we should decrease the count;
-        }
+//         int count=0; // storing count of element
+//         int ele=0; // element
+//         for(int i :nums){
+//             if(count==0){ // if count becomes zeros after cancelling out with different element
+//             count =1;  // then we should initialized count with 1 and
+//             ele= i; // then we ahould store that in ele.
+//             }
+//             else if(ele==i) // if ele is same then we shoyuld increment the counter;
+//             count++;
+//             else
+//             count--; // if different ele encounter then we should decrease the count;
+//         }
 
 
-return ele;
-
-
+// return ele;
 
 
       // tc = O(N) as we are traversing the array once and sc of O(n) as we are using map
-        // Map<Integer,Integer> m = new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     m.put(nums[i],m.getOrDefault(nums[i],0)+1);
-        // }
-        // int max=nums[0];
-        // for(int i: m.keySet()){
-        //     if(m.get(i)>(n/2))
-        //    { max=i;
-        //     break;
-        //     }
-        // }
-        // return max;
-     
-
+    Map<Integer,Integer> m = new HashMap<>();
+    int n = nums.length;
+        for(int num : nums){
+            m.put(num,m.getOrDefault(num,0)+1);
+            if(m.get(num)>n/2)
+                return num;
+        }
+    return -1;
 
         //   int f[]= new int[n];
         // for(int i=0;i<n;i++){
